d8bf0600db0a577852313ce343b030db8e027e24
Chrd26 <christosdouris@outlook.com>
2025-01-26 09:08:27 +0200

    Added xlsxio to the project, did a hello world project for initialisation purposes, need to learn some better C before proceeding

---
+++ build/CMakeFiles/SpreadsheetParser.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SpreadsheetParser.dir/src/main.c.o"
+  "CMakeFiles/SpreadsheetParser.dir/src/main.c.o.d"
+  "SpreadsheetParser"
+  "SpreadsheetParser.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SpreadsheetParser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
